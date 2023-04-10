ab63bdd46eca162ca3ca2ca7fec4bc91ff92ad2b
Ramees H <khramees.ramees@gmail.com>
2023-04-10 16:19:43 +0530

    started parkinglot app

---
+++ app/parkinglot/tests/test_parking_lot_api.py
@@ -0,0 +1,8 @@
+"""
+Test for parkinglot APIs
+"""
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
